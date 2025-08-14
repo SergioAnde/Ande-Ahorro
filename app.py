ec8f0dd1fd42862e93af1cfe7899f6883dbd5b44
SergioAnde <sergio.achon@fiuna.edu.py>
2025-08-13 21:31:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -125,5 +125,3 @@ def index():
 
     return render_template('index.html', electrodomesticos=electrodomesticos_db, resultado=resultado)
 
-if __name__ == "__main__":
-    app.run(port=5000, debug=True)
